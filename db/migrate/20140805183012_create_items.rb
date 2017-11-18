c28eb0007fb31c227e2dfa6b7fd32d87098c596b
Kevin Yongchul Kim <kevikim33@gmail.com>
2017-11-17 22:59:09 -0800

    db and model set up properly (I think...)

--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,10 @@
 class CreateItems < ActiveRecord::Migration
   def change
-
+    create_table :items do |t|
+      t.integer :category_id
+      t.string :title
+      t.integer :inventory
+      t.decimal :price, precision: 7, scale: 2
+    end
   end
 end
